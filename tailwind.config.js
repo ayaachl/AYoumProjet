e5a73971fccf94afc3699a745b8e7387c40e9b7b
CHALLAL Aya <ayachallal10@gmail.com>
2025-01-10 07:50:33 +0100

    Initialisation du projet AYouM

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'], // Définir Roboto comme police principale
+      },
+    },
+  },
+};
+
+
